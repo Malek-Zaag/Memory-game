7b37ef1a589a0b50baab9faecc8f933041fa5179
Malek-Zaag <zaag.malek1@gmail.com>
2022-01-17 12:16:07 +0100

    making cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './images/cover.jpg'
 
 function App() {
+  const CardImages = [
+    { src: "/images/abricot.jfif" },
+    { src: "/images/banana.png" },
+    { src: "/images/carotte.jpg" },
+    { src: "/images/laitue.png" },
+    { src: "/images/peche.jpg" },
+    { src: "/images/pomme de terre.png" },
+    { src: "/images/pomme.png" },
+    { src: "/images/tomate.png" },
+  ]
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const shuffleCards = () => {
+    const shuffledCards = [...CardImages, ...CardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+    setCards(shuffledCards)
+    setTurns(0);
+    console.log(cards)
+    console.log(turns)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative" }}>
+      <div className="title">VegFruity Memory Game</div>
+      <button onClick={shuffleCards}>New Game</button>
+      <div>{cards.map((card) => (
+        <div>
+          <img src={card.src} alt="front"></img>
+        </div>
+      )
+      )}
+      </div>
     </div>
   );
 }
